bf10dc2eb431c9566c9f9e33fcfd011df026a645
yoonsw15 <yoonsw15@ucla.edu>
2015-04-24 17:06:19 -0700

    got rid of user for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create((created_at:'test', updated_at:'test', id:'123'))
 marvel_exchange = Exchange.create(name:'marvelExchange', thumburl:'http://www.insidethemagic.net/wp-content/uploads/2013/02/marvel.jpg')
 mario_exchange = Exchange.create(name:'marioExchange', thumburl:'http://screenshots.en.sftcdn.net/en/scrn/3334000/3334419/super-mario-galaxy-2-18-700x437.jpg')
 gameOfThrones = Exchange.create(name:'gameOfThonesExchange', thumburl:'http://www.geekpeeks.com/wp-content/uploads/2011/12/gameofthrones-title-WIDE.jpg')
