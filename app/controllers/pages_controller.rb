07782739f186b41260fdb82f7902f2242edd2e87
Tianyi <tianyixia55@gmail.com>
2015-06-03 22:29:37 -0700

    Prepare for large data test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
 
   def dashboard
       @photo = Photo.new
-  	  @myExchangeProfiles = ExchangeProfile.all.select { |e| e.user_id ==  current_user.id }
+  	  @myExchangeProfiles = ExchangeProfile.select { |e| e.user_id ==  current_user.id }
   	  @myExchanges = []
       for event in @myExchangeProfiles
       	item = Exchange.find_by(id: event.exchange_id) #use find by to avoid not found exception
